1008847dd8567a7f2bb753d0ab1f1a04628d4917
sorayaskavinski <soraya1br@gmail.com>
2026-08-04 21:35:50 -0300

    first part of html django

--- sales_app/views.py
+++ sales_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    """
+    Display the application's home page.
+    """
+
+    return render(request, "home.html")
